68114bb3f3fae80a787306db4d2e6038424fed65
김의현 <godwvp@naver.com>
2026-05-02 20:23:41 +0900

    fastapi-1 main.py 추가

---
+++ fastapi-1/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# 서버에 GET /hello 요청이 들어오면, root_handler를 실행한다.
+@app.get("/hello")
+def hello_handler():
+    return {"ping": "pong"}
+
+users = [
+    {"id": 1, "name": "alex"},
+    {"id": 2, "name": "bob"},
+    {"id": 3, "name": "chris"},
+]
+
+# 전체 사용자 조회 API
+@app.get("/users")
+def get_users_handlers():
+    return users
+
+# {user_id}번 사용자 조회 API
+@app.get("/users/{user_id}")
+def get_user_handler(user_id: int):
+    return users[user_id - 1]
